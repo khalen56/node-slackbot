dc86f4da1748219027394c27c77daf778cb2cd51
Gwendal LE DOUSSAL <gwendal.ledoussal@jymeo.net>
2016-10-19 11:57:37 +0200

    Post & delete route

--- controllers/mentions.js
+++ controllers/mentions.js
@@ -9,4 +9,35 @@ router.get('/', (req, res) => {
 	res.json({msg: "Voilà les réponses à mes mentions, maintenant casse toi.", result: global.mentions});
 });
 
+router.post('/', (req, res) => {
+	try {
+		if (req.body.mention) {
+			global.mentions.push(req.body.mention);
+			fs.writeFileSync(`${__dirname}/../.config/mentions.json`, JSON.stringify(global.mentions, null, '  '));
+			res.json({msg: "Ok."});
+		} else {
+			res.status(400).json({msg: "Mauvaise requête."});
+		}
+	} catch(e) {
+		console.log(e);
+		res.status(500).json({msg: "Erreur."});
+	}
+});
+
+router.delete('/:id', (req, res) => {
+	try {
+		const id = parseInt(req.params.id);
+		if (global.mentions[id]) {
+			global.mentions.splice(id, 1);
+			fs.writeFileSync(`${__dirname}/../.config/mentions.json`, JSON.stringify(global.mentions, null, '  '));
+			res.json({msg: "Ok."});
+		} else {
+			res.status(404).json({msg: "Introuvable, pd."});
+		}
+	} catch(e) {
+		console.log(e);
+		res.status(500).json({msg: "Erreur."});
+	}
+});
+
 module.exports = router;
